e8e1fda20094c2821b9069144331dfb782d31dc0
Timothy Tran <timotran@gmail.com>
2017-03-21 05:37:44 -0700

    Add more code to JS and HTML

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 'use strict';
+
+var clicked = false;
+
+function SwitchPic(pic) {
+    if (!clicked) {
+        pic.src = "img/bag.jpg";
+        clicked = true;
+    } else {
+        pic.src = "img/pet-sweep.jpg";
+        clicked = false;
+    }
+}
