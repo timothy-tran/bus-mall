d1bb1bfa3c783f40f1e1ad4ed5d22b211c8b64d7
Timothy Tran <timotran@gmail.com>
2017-03-23 09:33:52 -0700

    update files

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'user strict';
+'use strict';
 
 var imageArray = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
 
