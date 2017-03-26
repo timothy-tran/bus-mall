ed4eceedab71292462b7e7f13ea730054f0bef66
Timothy Tran <timotran@gmail.com>
2017-03-25 22:09:03 -0700

    styling with css

--- table.js
+++ table.js
@@ -15,22 +15,46 @@ function calPercentage () {;
     tabPercent.push(Math.floor(numOfClick[i] / numTimeShown[i] * 100));
   }
 };
+function tableHead() {
+  var tabHead = document.createElement('tr');
+  tabHead.setAttribute('id', 'tabRow');
+  var tdItem = document.createElement('td');
+  tdItem.textContent = 'Item';
+  tabHead.appendChild(tdItem);
+  var tdView = document.createElement('td');
+  tdView.textContent = 'Views';
+  tabHead.appendChild(tdView);
+  var tdClick = document.createElement('td');
+  tdClick.textContent = 'Clicks';
+  tabHead.appendChild(tdClick);
+  var tdPerct = document.createElement('td');
+  tdPerct.textContent = '% of Clicks When Viewed';
+  tabHead.appendChild(tdPerct);
+  var tdRec = document.createElement('td');
+  tdRec.textContent = 'Recommanded?';
+  tabHead.appendChild(tdRec);
+  myTable.appendChild(tabHead);
+}
 
 function createTable() {
   for (var i = 0; i < productName.length; i++) {
     var myTRow = document.createElement('tr');
     var tdName = document.createElement('td');
-    tdName.setAttribute('class', 'product');
+    if (i % 2) {
+      tdName.setAttribute('class', 'line1');
+    } else {
+      tdName.setAttribute('class', 'line2');
+    }
     tdName.textContent = productName[i];
     myTRow.appendChild(tdName);
     var tdClick = document.createElement('td');
-    tdClick.textContent = 'Click ' + numOfClick[i];
+    tdClick.textContent = numOfClick[i];
     myTRow.appendChild(tdClick);
     var tdShown = document.createElement('td');
-    tdShown.textContent = 'Time Shown ' + numTimeShown[i];
+    tdShown.textContent = numTimeShown[i];
     myTRow.appendChild(tdShown);
     var tdPerct = document.createElement('td');
-    tdPerct.textContent = 'Percent ' + tabPercent[i];
+    tdPerct.textContent = tabPercent[i] + '%';
     myTRow.appendChild(tdPerct);
     var tdRecommend = document.createElement('td');
     if (tabPercent[i] > 40) {
@@ -44,4 +68,5 @@ function createTable() {
 }
 
 calPercentage();
+tableHead();
 createTable();
